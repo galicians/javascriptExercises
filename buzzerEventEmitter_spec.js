d93d9d908f92d221a3edd381cb75f48e0ce51af7
galicians <galicians@gmail.com>
2014-12-24 18:53:55 +0000

    test buzzerEvent passing

---
+++ buzzerEventEmitter_spec.js
@@ -0,0 +1,20 @@
+var should = require("should");
+var Buzzer = require('./buzzerEventEmitter');
+
+
+describe('the buzzer object', function() {
+    var buzzer;
+    var called;
+
+    beforeEach(function(){
+        buzzer = new Buzzer();
+        called = null;
+    });
+
+    it("emits a 'buzz' when pressed", function(){
+        buzzer.on('buzz', function() { called = true } );
+        called = false
+        buzzer.press()
+        called.should.equal(true)
+    });
+});
